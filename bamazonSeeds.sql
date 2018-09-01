1b053d6aae652c86ac23e658cd96bdc316884eff
Elizabeth <avrora7@yahoo.com>
2018-09-01 14:29:52 -0700

    tables created and filled in

---
+++ bamazonSeeds.sql
@@ -0,0 +1,17 @@
+USE bamazon_DB;
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("Retractable leash", "Pet supplies", 11.50, 25),
+    ("Ceramic cup", "Kitchen ware", 2.75, 80),
+    ("No show socks", "Hosiery", 4.25, 60),
+    ("Dry liver pieces", "Pet supplies", 8.50, 15),
+    ("Ceramic pan", "Kitchen ware", 14.00, 9),
+    ("Yoga socks", "Hosiery", 5.00, 5),
+    ("Cotton tshirt", "Mens fashion", 17.75, 20),
+    ("Linen pants", "Mens fashion", 39.00, 7),
+    ("Led collar", "Pet supplies", 9.50, 14),
+    ("7th grade math", "Books", 18.25, 5),
+    ("Paris hidden gems", "Books", 12.50, 11);
+
